c279bf7d7342326a53ea06c20b04c0aeef6726c5
HiiD <salomone.marco@outlook.it>
2021-02-23 10:10:07 +0100

    Fixed the stop feature

--- src/application/ChronoController.java
+++ src/application/ChronoController.java
@@ -4,37 +4,39 @@ public class ChronoController implements Controller {
 
     private static final long STEP = 1000;
 
-    private final ChronoAgent chronoAgent;
+    private ChronoAgent chronoAgent;
     private final MillisecondsTimer timer = new SimpleMillisecondsTimer(ChronoController.STEP);
-    private final NotificationsAgent notificationsAgent;
-
-    public ChronoController() {
-        this.notificationsAgent = new NotificationsAgent(this.timer);
-        this.chronoAgent = new ChronoAgent(ChronoController.STEP, this.timer);
-    }
+    private NotificationsAgent notificationsAgent;
+    private final EventListenerList<TimerTickListener> chronoListeners = new EventListenerList<>();
+    private final EventListenerList<NotificationListener> notificationsListeners = new EventListenerList<>();
 
     @Override
     public void addChronoListener(final TimerTickListener listener) {
-        this.chronoAgent.addEventListener(listener);
+        this.chronoListeners.addListener(listener);
     }
 
     @Override
     public void removeChronoListener(final TimerTickListener listener) {
-        this.chronoAgent.removeEventListener(listener);
+        this.chronoListeners.removeListener(listener);
     }
 
     @Override
     public void addNotificationsListener(final NotificationListener listener) {
-        this.notificationsAgent.addEventListener(listener);
+        this.notificationsListeners.addListener(listener);
     }
 
     @Override
     public void removeNotificationsListener(final NotificationListener listener) {
-        this.notificationsAgent.removeEventListener(listener);
+        this.notificationsListeners.removeListener(listener);
     }
 
     @Override
     public void notifyStarted() {
+        this.chronoAgent = new ChronoAgent(ChronoController.STEP, this.timer);
+        this.notificationsAgent = new NotificationsAgent(this.timer);
+        this.chronoListeners.getListeners().forEach(listener -> this.chronoAgent.addEventListener(listener));
+        this.notificationsListeners.getListeners()
+                .forEach(listener -> this.notificationsAgent.addEventListener(listener));
         this.chronoAgent.start();
         this.notificationsAgent.start();
     }
